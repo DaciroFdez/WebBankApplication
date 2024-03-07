954955046f9110197a9d502e76ee4b7e2abc75b9
Alejandro <fernandezdalejo@gmail.com>
2024-03-06 22:08:11 -0500

    Se añade el Controller para tipo de Usuario y el DTO de user

---
+++ WebApiBank/Controllers/UserTypeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiBank.DataBase;
+using WebApiBank.DTOs;
+using WebApiBank.Models;
+
+namespace WebApiBank.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTypeController : ControllerBase
+    {
+        private readonly BankApiDB _dbContex;
+
+        public UserTypeController(BankApiDB dbContext)
+        {
+            _dbContex = dbContext;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateUserType([FromBody] UserTypeDTO userTypeDTO)
+        {
+            try
+            {
+                var userType = new UserType()
+                {
+                    UserTypeName = userTypeDTO.UserTypeName
+                };
+
+                _dbContex.UserTypes.Add(userType);
+                await _dbContex.SaveChangesAsync();
+                return Ok("Creado");
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ WebApiBank/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiBank.DTOs
+{
+    public class UserDTO
+    {
+        public string DocumentNumber { get; set; }
+        public string NameUser { get; set; }
+        public int DocumentTypeId { get; set; }
+        public int UserTypeId { get; set; }
+        public int SiteId { get; set; }
+    }
+}
